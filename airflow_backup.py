79263c1526d2a864596a8eaac060df93eff8d6b3
rotemfo@gmail.com <RotemFogel#0)^>
2020-06-03 12:09:27 -0400

    airflow backup/restore support encryption

---
+++ airflow_backup.py
@@ -0,0 +1,67 @@
+import json
+
+from airflow import settings
+from airflow.models import Variable, Connection
+from airflow.models.crypto import get_fernet
+from smart_open import open
+
+env = 'Local'
+data_bucket = 'my_bucket'
+fernet = get_fernet()
+session = settings.Session()
+
+
+def _write_to_s3(uri: str, records) -> None:
+    with open(uri=uri, mode='wb') as s3_file:
+        if records:
+            s3_file.write((json.dumps(records)).encode())
+
+
+def _backup_variables() -> None:
+    variables = session.query(Variable).all()
+    records = {}
+    for variable in variables:
+        parts = str(variable).split(':')
+        trimmed = list(map(lambda x: x.strip(), parts))
+        key = trimmed[0]
+        value = fernet.decrypt(bytes(str(trimmed[1:]), 'utf-8')).decode()
+        records.update({key: value})
+
+    uri = 's3://{bucket}/airflow-backup/{env}/date_={date}/{what}.json.gz'.format(bucket=data_bucket,
+                                                                                  env=env,
+                                                                                  date=execution_date,
+                                                                                  what='variables', )
+    _write_to_s3(uri, records)
+    print("Backed up Airflow [{env}] variables to S3 ({uri})".format(env=env, uri=uri))
+
+
+def _backup_connections() -> None:
+    connections = session.query(Connection).all()
+    records = []
+    for connection in connections:
+        password: str = None
+        if connection.password:
+            password = str(fernet.encrypt(bytes(connection.password, 'utf-8')))
+        connection = {
+            'conn_id': connection.conn_id,
+            'extra': connection.extra,
+            'is_extra_encrypted': connection.is_extra_encrypted,
+            'conn_type': connection.conn_type,
+            'login': connection.login,
+            'password': password,
+            'schema': connection.schema,
+            'port,': connection.port,
+        }
+        records.append(connection)
+
+    uri = 's3://{bucket}/airflow-backup/{env}/date_={date}/{what}.json.gz'.format(bucket=data_bucket,
+                                                                                  env=env,
+                                                                                  date=execution_date,
+                                                                                  what='connections', )
+    _write_to_s3(uri, records)
+    print("Backed up Airflow [{env}] connections to S3 ({uri})".format(env=env, uri=uri))
+
+
+execution_date = "2020-06-03"
+# _backup_variables(execution_date)
+_backup_connections(execution_date)
--- airflow_restore.py
+++ airflow_restore.py
@@ -5,21 +5,21 @@ from airflow.models import Variable, Connection
 from airflow.models.crypto import get_fernet
 from smart_open import open
 
-env = Variable.get('env')
-data_bucket = Variable.get('data_bucket')
+env = 'Local'
+data_bucket = 'my_bucket'
 fernet = get_fernet()
 session = settings.Session()
 
 
-def _read_from_s3(uri):
+def _read_from_s3(uri: str) -> list:
     records = []
-    with open(uri=uri, mode='rb') as s3_file:
+    with open(uri=uri, mode='r') as s3_file:
         for line in s3_file:
-            records.append()
+            records.append(str(line))
     return records
 
 
-def _restore_variables(execution_date):
+def _restore_variables() -> None:
     uri = 's3://{bucket}/airflow-backup/{env}/date_={date}/{what}.json.gz'.format(bucket=data_bucket,
                                                                                   env=env,
                                                                                   date=execution_date,
@@ -28,12 +28,13 @@ def _restore_variables(execution_date):
     raw_variables = _read_from_s3(uri)
     variables = json.loads(''.join(raw_variables))
     for k, v in variables.items():
-        Variable.set(k, v)
+        if not Variable.get(k):
+            Variable.set(k, v)
 
-    print("Restored Airflow [{env}] {len} variables to S3 ({uri})".format(env=env, len=len(variables), uri=uri))
+    print("Restored Airflow [{env}] {len} variables from S3 ({uri})".format(env=env, len=len(variables), uri=uri))
 
 
-def _restore_connections(execution_date):
+def _restore_connections() -> None:
     uri = 's3://{bucket}/airflow-backup/{env}/date_={date}/{what}.json.gz'.format(bucket=data_bucket,
                                                                                   env=env,
                                                                                   date=execution_date,
@@ -41,25 +42,68 @@ def _restore_connections(execution_date):
     raw_connections = _read_from_s3(uri)
     connections = json.loads(''.join(raw_connections))
 
+    restored: int = 0
     for connection in connections:
-        conn_id = conn_id = connection['conn_id']
-        conn_exists = session.query(Connection).filter_by(conn_id=conn_id).first()
-        if not conn_exists:
-            conn2save = Connection(
-                conn_id=conn_id,
-                extra=connection['extra'],
-                is_extra_encrypted=connection['is_extra_encrypted'],
-                conn_type=connection['conn_type'],
-                login=connection['login'],
-                password=connection['password'],
-                schema=connection['schema'],
-                port=connection['port'],
-            )
-            session.save(conn2save)
+        conn_id = connection['conn_id']
+        # conn = session.query(Connection).filter_by(conn_id=conn_id).first()
+        # if conn:
+        #     session.delete(conn)
+        #     session.commit()
 
-    print("Backed up Airflow [{env}] connections to S3 ({uri})".format(env=env, uri=uri))
+        password: str = None
+        try:
+            if connection['password']:
+                password = fernet.decrypt(bytes(connection['password'], 'utf-8')).decode('utf-8')
+        except KeyError:
+            pass
+        port: int = None
+        try:
+            port = connection['port']
+        except KeyError:
+            pass
+        host: str = None
+        try:
+            host = connection['host']
+        except KeyError:
+            pass
+        login: str = None
+        try:
+            login = connection['login']
+        except KeyError:
+            pass
+        schema: str = None
+        try:
+            schema = connection['schema']
+        except KeyError:
+            pass
+        extra: str = None
+        try:
+            extra = connection['extra']
+        except KeyError:
+            pass
+        uri: str = None
+        try:
+            uri = connection['uri']
+        except KeyError:
+            pass
+        conn2save = Connection(
+            conn_id=conn_id,
+            conn_type=connection['conn_type'],
+            host=host,
+            login=login,
+            password=password,
+            schema=schema,
+            port=port,
+            extra=extra,
+            uri=uri
+        )
+        session.merge(conn2save)
+        session.commit()
+        restored += 1
+    if restored > 0:
+        print("Restored Airflow [{env}] {len} connections from S3 ({uri})".format(env=env, len=restored, uri=uri))
 
 
-execution_date = "2020-06-01"
+execution_date = "2020-06-03"
 _restore_variables(execution_date)
 _restore_connections(execution_date)
