4179c0702e7ba02cf18bb84b5410ff6c479942e9
Rotem Fogel <rotem@seekingalpha.com>
2020-08-16 13:36:12 +0300

    :|

--- partitioner/partitioner.py
+++ partitioner/partitioner.py
@@ -1,20 +1,8 @@
+import sys
+
 import boto3
 
-
-def athena_query(client, params):
-    response = client.start_query_execution(
-        QueryString=params["query"],
-        QueryExecutionContext={
-            'Database': params['database']
-        },
-        ResultConfiguration={
-            'OutputLocation': 's3://' + params['bucket'] + '/' + params['path']
-        }
-    )
-    return response
-
-
-params = {
+_params = {
     'region': 'us-west-2',
     'database': 'dbl',
     'bucket': 'aws-athena-query-results-744522205193-us-west-2',
@@ -22,31 +10,51 @@ params = {
     'query': '???'
 }
 
-session = boto3.Session()
-client = session.client('athena')
-f = open('athena.sql')
-lines = f.readlines()
-f.close()
 
-for line in lines:
-    params.update({'query': line})
-    execution = athena_query(client, params)
+def athena_query(client):
+    response = client.start_query_execution(
+        QueryString=_params["query"],
+        QueryExecutionContext={
+            'Database': _params['database']
+        },
+        ResultConfiguration={
+            'OutputLocation': 's3://' + _params['bucket'] + '/' + _params['path']
+        }
+    )
+    return response
 
-    execution_id = execution['QueryExecutionId']
-    state = 'RUNNING'
-    while state in ['RUNNING']:
-        response = client.get_query_execution(QueryExecutionId=execution_id)
-        state = response['QueryExecution']['Status']['State']
-        statistics = response['QueryExecution']['Statistics']
-        print('id: {}, state: {}, {} millis'.format(execution_id, state, statistics['TotalExecutionTimeInMillis']))
-        if state == 'FAILED':
-            change_reason = str(response['QueryExecution']['Status']['StateChangeReason'])
-            if 'Partition already exists' in change_reason:
-                print('{}: partition already exists', params['query'])
+
+def main(file):
+    f = open(file)
+    lines = f.readlines()
+    f.close()
+
+    session = boto3.Session()
+    client = session.client('athena')
+
+    for line in lines:
+        _params.update({'query': line})
+        execution = athena_query(client, _params)
+
+        execution_id = execution['QueryExecutionId']
+        state = 'RUNNING'
+        while state in ['RUNNING']:
+            response = client.get_query_execution(QueryExecutionId=execution_id)
+            state = response['QueryExecution']['Status']['State']
+            statistics = response['QueryExecution']['Statistics']
+            print('id: {}, state: {}, {} millis'.format(execution_id, state, statistics['TotalExecutionTimeInMillis']))
+            if state == 'FAILED':
+                change_reason = str(response['QueryExecution']['Status']['StateChangeReason'])
+                if 'Partition already exists' in change_reason:
+                    print('{}: partition already exists', _params['query'])
+                    break
+                else:
+                    print(change_reason)
+                    break
+            elif state == 'SUCCEEDED':
                 break
-            else:
-                print(change_reason)
-                break
-        elif state == 'SUCCEEDED':
-            break
-        # time.sleep(1)
+            # time.sleep(1)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
