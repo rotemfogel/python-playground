ce37607fb841f9fd4e195af38c58519930acb8e5
Rotem Fogel <rotem@seekingalpha.com>
2022-05-31 11:20:59 +0300

    ¯\_(ツ)_/¯

--- airfart/everflow.py
+++ airfart/everflow.py
@@ -1,13 +1,25 @@
 import json
 import os
 
+from airflow import AirflowException
 from dotenv import load_dotenv
 
 from airfart.everflow.everflow_operator import EverFlowOperator
 
 if __name__ == '__main__':
     load_dotenv()
-    records_str = os.getenv('everflow_records')
-    records = json.loads(records_str)
-    everflow = EverFlowOperator(records=records)
-    everflow.execute()
+    try:
+        records_str = os.getenv('everflow_records')
+        records = json.loads(records_str)
+        everflow = EverFlowOperator(records=records)
+        everflow.execute()
+    except AirflowException:
+        pass
+
+    try:
+        records_str = os.getenv('everflow_historical_records')
+        records = json.loads(records_str)
+        everflow = EverFlowOperator(records=records, report_click=False)
+        everflow.execute()
+    except AirflowException:
+        pass
